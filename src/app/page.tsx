f74263a3caabaa5ccbed5da2bec1855dd35414aa
Alejandro Suárez <alejandro-hdez115@outlook.com>
2023-11-01 12:42:55 -0600

    Add some comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import Dropdown from '../components/Dropdown/Dropdown';
 
 const Home = () => {
   return (
-    <div>
+    <div className='flex flex-col'>
+      {/* Header */}
       <div className='flex bg-orange-600 text-neutral-50 text-center py-3 px-3 justify-between items-center'>
         <h1 className='font-bold text-3xl uppercase'>
           Monitoreo de entrada - CUCEI
@@ -12,6 +13,8 @@ const Home = () => {
           items={[{ id: 0, name: 'Puerta 1' }]}
         />
       </div>
+      {/* Content */}
+      <div>{/* TODO: Add content */}</div>
     </div>
   );
 };
